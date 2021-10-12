4eed985771466f6104f1e6087e5b81fb92b3b593
Marsel Gabbasov <marsel13_98@mail.ru>
2021-10-12 20:12:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $(".rate-yo").rateYo({
+        ratedFill: "#FADF94",
+        spacing: "7px",
+        starWidth: "50px",
+        fullStar: true,
+        normalFill: "#C4C4C4"
+    });
+
+});
